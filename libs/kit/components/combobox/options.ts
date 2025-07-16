b6887a1bc8ea44ba8727516a83cef9bdfbd99783
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-16 18:09:44 +0200

    feat(kit): add `stringifyFn` to `XComboboxOptions`

--- libs/kit/components/combobox/options.ts
+++ libs/kit/components/combobox/options.ts
@@ -1,14 +1,16 @@
-import { inject, InjectionToken } from '@angular/core';
+import { FactoryProvider, inject, InjectionToken } from '@angular/core';
 import { X_COMPARATOR, XComparator } from '@mixin-ui/kit/providers';
 
-export interface XComboboxOptions {
+export interface XComboboxOptions<T = any> {
+  readonly compareFn: XComparator;
+  readonly stringifyFn: (value: T | string | null) => string;
   readonly strict: boolean;
-  readonly compareFn?: XComparator;
 }
 
-const defaultOptionsFactory = () => ({
+const defaultOptionsFactory = (): XComboboxOptions => ({
   compareFn: inject(X_COMPARATOR),
   strict: true,
+  stringifyFn: value => (value ? String(value) : ''),
 });
 
 export const X_COMBOBOX_OPTIONS = new InjectionToken<XComboboxOptions>('COMBOBOX_OPTIONS', {
