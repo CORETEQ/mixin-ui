d7743c5c33e07aaacd45471127b353a5d324f3f7
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-21 10:42:33 +0200

    chore: update control.ts

--- libs/kit/directives/control/control.ts
+++ libs/kit/directives/control/control.ts
@@ -1,12 +1,4 @@
-import {
-  Directive,
-  ElementRef,
-  forwardRef,
-  inject,
-  INJECTOR,
-  OnDestroy,
-  Renderer2,
-} from '@angular/core';
+import { Directive, ElementRef, forwardRef, inject, INJECTOR, Renderer2 } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl, NgModel } from '@angular/forms';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { fromEvent, map, Observable } from 'rxjs';
@@ -28,7 +20,7 @@ import { X_CONTROL_ACCESSOR } from './providers';
     '(blur)': 'onTouched();',
   },
 })
-export class XControl<T> implements ControlValueAccessor, OnDestroy {
+export class XControl<T> implements ControlValueAccessor {
   readonly #accessor = inject(X_CONTROL_ACCESSOR, { optional: true });
   readonly #injector = inject(INJECTOR);
   readonly #el = inject(ElementRef<HTMLElement>).nativeElement;
@@ -38,10 +30,6 @@ export class XControl<T> implements ControlValueAccessor, OnDestroy {
   protected onTouched = EMPTY_FN;
 
   constructor() {
-    if (this.#accessor?.handleControlInit) {
-      this.#accessor.handleControlInit(this.#el);
-    }
-
     this.valueChanges.pipe(takeUntilDestroyed()).subscribe(value => {
       if (this.modelValue === value) {
         return;
@@ -73,15 +61,9 @@ export class XControl<T> implements ControlValueAccessor, OnDestroy {
     this.#r2.setProperty(this.#el, 'disabled', disabled);
   }
 
-  ngOnDestroy(): void {
-    if (this.#accessor?.handleControlDestroy) {
-      this.#accessor.handleControlDestroy();
-    }
-  }
-
   private get valueChanges(): Observable<T> {
     if (this.#accessor) {
-      return this.#accessor.valueChanges;
+      return this.#accessor.controlChanges;
     } else {
       return fromEvent(this.#el, 'input').pipe(map(() => this.#el.value));
     }
