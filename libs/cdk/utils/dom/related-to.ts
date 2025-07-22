2448628f13e4892bda48218e019fe55e4f526403
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-22 23:06:10 +0200

    feat(cdk): implement new overloads for `relatedTo` fn

--- libs/cdk/utils/dom/related-to.ts
+++ libs/cdk/utils/dom/related-to.ts
@@ -1,5 +1,20 @@
 import { isElement } from './is-element';
 
-export function relatedTo(e: MouseEvent | FocusEvent, el: HTMLElement | null): boolean {
-  return isElement(e.relatedTarget) && !!el?.contains(e.relatedTarget);
+export function relatedTo(e: MouseEvent | FocusEvent, el: HTMLElement | null): boolean;
+export function relatedTo(e: MouseEvent | FocusEvent, selector: string): boolean;
+export function relatedTo(
+  e: MouseEvent | FocusEvent,
+  elOrSelector: HTMLElement | null | string
+): boolean {
+  const { relatedTarget } = e;
+
+  if (!isElement(relatedTarget)) {
+    return false;
+  }
+
+  if (typeof elOrSelector === 'string') {
+    return !!relatedTarget.closest(elOrSelector);
+  }
+
+  return !!elOrSelector?.contains(relatedTarget);
 }
