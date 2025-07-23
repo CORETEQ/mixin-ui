36a1452578b0d73bde475fb64c85514754e7023b
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-23 21:23:18 +0200

    feat(kit): add `handleListboxActiveDescendant` to `XListboxAccessor`, update typing

--- libs/kit/components/listbox/providers.ts
+++ libs/kit/components/listbox/providers.ts
@@ -1,16 +1,20 @@
 import { ExistingProvider, InjectionToken, Signal, Type } from '@angular/core';
-import { XComparator } from '@mixin-ui/kit/providers';
-import { Observable } from 'rxjs';
+import type { XComparator } from '@mixin-ui/kit/providers';
+import type { Observable } from 'rxjs';
+import type { XOption } from './option';
 
 export interface XListboxAccessor<T = any> {
   readonly selection: Signal<T | readonly T[] | null>;
+
   readonly multiple?: Signal<boolean>;
   readonly comparator?: Signal<XComparator<T>>;
   readonly keyboardEvents?: Observable<KeyboardEvent>;
 
   handleListboxValue(value: readonly T[]): void;
 
-  handleListboxOptions?(options: readonly T[] | null): void;
+  handleListboxOptions?(options: readonly XOption<T>[] | null): void;
+
+  handleListboxActiveDescendant?(option: XOption<T> | null): void;
 }
 
 export const X_LISTBOX_ACCESSOR = new InjectionToken<XListboxAccessor>('LISTBOX_ACCESSOR');
