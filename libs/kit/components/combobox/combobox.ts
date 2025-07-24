56012aff41ad27986040ddd4c6906b9cd4cb53c6
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-24 14:33:46 +0200

    fix(kit): fix `XCombobox` popover when state is disabled

--- libs/kit/components/combobox/combobox.ts
+++ libs/kit/components/combobox/combobox.ts
@@ -11,6 +11,7 @@ import {
   input,
   Renderer2,
   signal,
+  untracked,
   ViewEncapsulation,
 } from '@angular/core';
 import { fromEvent, Subject } from 'rxjs';
@@ -68,19 +69,23 @@ import { X_COMBOBOX_OPTIONS } from './options';
 export class XCombobox<T> implements XControlAccessor<T | string | null>, XListboxAccessor<T> {
   readonly #opt = inject(X_COMBOBOX_OPTIONS);
   readonly #popover = inject(XPopoverTarget);
+  readonly #input = inject(XInput);
   readonly #el = inject(ElementRef).nativeElement;
   readonly #r2 = inject(Renderer2);
   readonly #modelChanges = new Subject<T | string | null>();
 
-  readonly input = contentChild.required(XControl, { read: ElementRef });
   readonly open = this.#popover.open;
+  readonly native = contentChild.required(XControl, { read: ElementRef });
+  readonly stringify = input(this.#opt.stringify);
+  readonly matcher = input(this.#opt.matcher);
   readonly _comparator = input(this.#opt.comparator, { alias: 'comparator' });
-  readonly _stringify = input(this.#opt.stringify, { alias: 'stringify' });
-  readonly _matcher = input(this.#opt.matcher, { alias: 'matcher' });
   readonly strict = input(this.#opt.strict, { transform: booleanAttribute });
   readonly key = input<PropertyKey>();
   readonly comparator = computed(() =>
     this.key() != null ? createKeyComparator(this.key()!) : this._comparator()
+  );
+  readonly disabled = computed(
+    () => this.#input.state()?.disabled || this.#input.state()?.readOnly
   );
 
   /** @internal */
@@ -97,24 +102,32 @@ export class XCombobox<T> implements XControlAccessor<T | string | null>, XListb
   #options: readonly XOption<T>[] | null = null;
 
   constructor() {
+    watch(this.open, open => {
+      if (!open && this.strict() && this.nativeValue !== '' && !this.hasOption(this.nativeValue)) {
+        this.updateModelValue(null);
+        this.updateSelection(null);
+        this.resetNativeValue();
+      }
+    });
+
     effect(() => {
-      const id = this.#activeDescendant();
-      const input = this.input().nativeElement;
+      const input = this.nativeElement;
+      const activeId = this.#activeDescendant();
       const open = this.open();
 
       this.#r2.setAttribute(input, 'role', 'combobox');
       this.#r2.setAttribute(input, 'autocomplete', 'false');
       this.#r2.setAttribute(input, 'aria-haspopup', 'listbox');
-      this.#r2.setAttribute(input, 'aria-activedescendant', id || '');
+      this.#r2.setAttribute(input, 'aria-activedescendant', activeId || '');
       this.#r2.setAttribute(input, 'aria-expanded', String(open));
     });
 
-    watch(this.open, open => {
-      if (!open && this.strict() && this.nativeValue !== '' && !this.hasOption(this.nativeValue)) {
-        this.updateModel(null);
-        this.updateListbox(null);
-        this.updateNative('', { inputType: 'deleteContent', data: null });
-      }
+    effect(() => {
+      const disabled = this.disabled();
+
+      untracked(() => {
+        this.#popover.setDisabled(disabled);
+      });
     });
   }
 
@@ -134,32 +147,39 @@ export class XCombobox<T> implements XControlAccessor<T | string | null>, XListb
 
   /** @internal */
   handleInput(e: InputEvent): void {
-    if (e.target !== this.inputEl) {
+    if (e.target !== this.nativeElement) {
       return;
     }
 
     this.togglePopover(true);
 
     const option = this.findOption(this.nativeValue);
-    this.updateModel(option || this.nativeValue);
-    this.updateListbox(option ? option : null);
-    if (option) {
-      this.updateNative(this.stringify(option));
+
+    if (option && !option.disabled) {
+      const value = option.value();
+
+      this.updateModelValue(value);
+      this.updateSelection(value);
+      this.updateNativeValue(value);
+    } else {
+      this.updateModelValue(this.nativeValue);
+      this.updateSelection(null);
     }
   }
 
   /** @internal */
-  handleListboxValue(options: readonly T[]): void {
-    const value = options.at(0) || null;
-    this.updateModel(value);
-    this.updateNative(this.stringify(value));
-    this.updateListbox(value);
+  handleControlValue(value: T | string | null): void {
+    this.updateNativeValue(value);
+    this.updateSelection(value === '' ? null : (value as T));
   }
 
   /** @internal */
-  handleControlValue(value: T | string | null): void {
-    this.updateNative(this.stringify(value));
-    this.updateListbox(value === '' ? null : (value as T));
+  handleListboxSelection(values: readonly T[]): void {
+    const value = values.at(0) || null;
+
+    this.updateModelValue(value);
+    this.updateNativeValue(value);
+    this.updateSelection(value);
   }
 
   /** @internal */
@@ -167,7 +187,8 @@ export class XCombobox<T> implements XControlAccessor<T | string | null>, XListb
     this.#options = options;
 
     if (this.#options?.length) {
-      this.updateListbox(this.findOption(this.nativeValue) || null);
+      const option = this.findOption(this.nativeValue);
+      this.updateSelection(option && !option?.disabled ? option.value() : null);
     }
   }
 
@@ -176,43 +197,51 @@ export class XCombobox<T> implements XControlAccessor<T | string | null>, XListb
     this.#activeDescendant.set(option?.id() || null);
   }
 
-  private get inputEl(): HTMLInputElement {
-    return this.input().nativeElement;
+  private get nativeElement(): HTMLInputElement {
+    return untracked(this.native).nativeElement;
   }
 
   private get nativeValue(): string {
-    return this.inputEl.value;
+    return this.nativeElement.value;
   }
 
   private hasOption(value: string): boolean {
-    return !!this.#options?.some(option => this.matcher(value, option.value()));
+    return !!this.#options?.some(option => this.matches(value, option));
   }
 
-  private findOption(value: string): T | null {
-    return this.#options?.find(option => this.matcher(value, option.value()))?.value() || null;
+  private findOption(value: string): XOption<T> | null {
+    return this.#options?.find(option => this.matches(value, option)) || null;
   }
 
-  private updateModel(value: T | string | null): void {
-    this.#modelChanges.next(value);
-  }
-
-  private updateListbox(value: T | null): void {
+  private updateSelection(value: T | null): void {
     this.selection.set(value);
   }
 
-  private updateNative(value: string, emitEvent?: InputEventInit): void {
-    this.inputEl.value = value;
-
-    if (emitEvent) {
-      this.inputEl.dispatchEvent(new InputEvent('input', emitEvent));
-    }
+  private updateModelValue(value: T | string | null): void {
+    this.#modelChanges.next(value);
   }
 
-  private stringify(value: T | string | null): string {
-    return this._stringify()(value);
+  private updateNativeValue(value: T | string | null): void {
+    const stringify = this.stringify();
+    this.nativeElement.value = stringify(value);
   }
 
-  private matcher(value: string, option: T): boolean {
-    return this._matcher()(value, this.stringify(option));
+  private resetNativeValue(): void {
+    this.nativeElement.value = '';
+
+    this.nativeElement.dispatchEvent(
+      new InputEvent('input', {
+        inputType: 'deleteContent',
+        data: null,
+      })
+    );
+  }
+
+  private matches(value: string, option: XOption<T>): boolean {
+    const matcher = this.matcher();
+    const stringify = this.stringify();
+    const optionValue = stringify(option.value());
+
+    return matcher(value, optionValue);
   }
 }
