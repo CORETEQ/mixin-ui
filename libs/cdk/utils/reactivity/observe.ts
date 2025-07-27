198d26a224a4b87bfb3819f06beaa5b7d2feda64
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-27 02:44:53 +0200

    docs(cdk): add `observe` docs

--- libs/cdk/utils/reactivity/observe.ts
+++ libs/cdk/utils/reactivity/observe.ts
@@ -13,6 +13,37 @@ export interface CreateObserveOptions {
   readonly injector?: Injector;
 }
 
+/**
+ * Registers a side effect with an explicitly declared dependency that is RxJS observable.
+ *
+ * ### Motivation
+ *
+ * Angular's built-in `effect()` function provides a declarative way to register side effects
+ * in the signal-based (pull-driven) reactive model, with automatic cleanup managed by the framework.
+ *
+ * In contrast, working with RxJS (push-driven) Observables requires manual subscription management,
+ * including explicit unsubscription logic to avoid memory leaks.
+ *
+ * This asymmetry can lead to inconsistencies in how developers manage reactive side effects.
+ * The `observe()` function bridges this gap by offering a similar API for Observables,
+ * enabling automatic cleanup via `DestroyRef`.
+ *
+ * @example
+ * ```ts
+ *
+ * constructor() {
+ *   // Traditional pattern with takeUntilDestroyed():
+ *   changes.pipe(takeUntilDestroyed()).subscribe(value => {
+ *     console.log('value:', value);
+ *   });
+ *
+ *   // Using observe() to abstract away cleanup logic:
+ *   observe(changes, value => {
+ *     console.log('value:', value);
+ *   });
+ * }
+ * ```
+ */
 export function observe<T>(
   source: Observable<T>,
   fn: ObserveCallback<T>,
