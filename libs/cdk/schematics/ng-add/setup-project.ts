34eeca038f027c616b06b63b4d6e40e3ab0ef0db
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-29 22:22:00 +0200

    feat(cdk): test schematics

--- libs/cdk/schematics/ng-add/setup-project.ts
+++ libs/cdk/schematics/ng-add/setup-project.ts
@@ -1,9 +1,13 @@
 import { chain, Rule } from '@angular-devkit/schematics';
-import { updateWorkspace } from '@schematics/angular/utility';
+import {
+  ProjectDefinition,
+  updateWorkspace,
+  WorkspaceDefinition,
+} from '@schematics/angular/utility';
 import { Schema } from './schema';
 
-const NG_CDK_STYLES_PATH = 'node_modules/@angular/cdk/overlay-prebuilt.css';
-const MIXIN_STYLES_PATH = 'node_modules/@mixin-ui/kit/styles/index.scss';
+const NG_CDK_STYLES_PATH = '@angular/cdk/overlay-prebuilt.css';
+const MIXIN_STYLES_PATH = '@mixin-ui/kit/styles/index.scss';
 
 export default function (options: Schema): Rule {
   return chain([addGlobalStylesToWorkspace(options)]);
@@ -11,30 +15,22 @@ export default function (options: Schema): Rule {
 
 function addGlobalStylesToWorkspace(options: Schema): Rule {
   return updateWorkspace(workspace => {
-    const projectsToProcess = getProjectsToProcess(workspace, options);
+    const projects = getProjectsToProcess(workspace, options);
 
-    if (projectsToProcess.length === 0) {
+    if (projects.length === 0) {
       throw new Error('No application projects found to process');
     }
 
-    projectsToProcess.forEach(projectName => {
-      const project = workspace.projects.get(projectName);
-
-      if (!project) {
-        throw new Error(`Project ${projectName} not found`);
-      }
-
+    projects.forEach(project => {
       const buildTarget = project.targets.get('build');
 
       if (!buildTarget || !buildTarget.options) {
-        throw new Error(`Cannot find build options for project ${projectName}`);
+        throw new Error(`Cannot find build options for project ${project}`);
       }
 
       const styles = (buildTarget.options.styles ?? []) as Array<string>;
 
-      const stylePathsToAdd = [NG_CDK_STYLES_PATH, MIXIN_STYLES_PATH];
-
-      stylePathsToAdd.forEach(path => {
+      [NG_CDK_STYLES_PATH, MIXIN_STYLES_PATH].forEach(path => {
         if (!styles.includes(path)) {
           styles.push(path);
         }
@@ -45,22 +41,22 @@ function addGlobalStylesToWorkspace(options: Schema): Rule {
   });
 }
 
-function getProjectsToProcess(workspace: any, options: Schema): string[] {
-  if (options.project) {
-    return [options.project];
+function getProjectsToProcess(
+  workspace: WorkspaceDefinition,
+  options: Schema
+): ProjectDefinition[] {
+  const { projects, extensions } = workspace;
+
+  const project = projects.get(options.project || String(extensions.defaultProject));
+
+  if (project) {
+    return [project];
   }
 
-  if (workspace.extensions?.defaultProject) {
-    return [workspace.extensions.defaultProject];
-  }
-
-  const applicationProjects: string[] = [];
-
-  workspace.projects.forEach((project: any, projectName: string) => {
-    if (project.projectType === 'application') {
-      applicationProjects.push(projectName);
+  return Array.from(projects.entries()).reduce<ProjectDefinition[]>((acc, [_, project]) => {
+    if (project.extensions.projectType === 'application') {
+      acc.push(project);
     }
-  });
-
-  return applicationProjects;
+    return acc;
+  }, []);
 }
