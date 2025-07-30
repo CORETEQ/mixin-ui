8a7fc9fad96809ee57d0c294f5c438a1e7dbf0ef
Vyacheslav Borodin <vb-onyx@proton.me>
2025-07-30 16:56:53 +0200

    ci: setup

--- scripts/release.ts
+++ scripts/release.ts
@@ -39,10 +39,7 @@ export const MIXIN_UI_VERSION = '${newVersion}';
 `;
     fs.writeFileSync(versionFilePath, content);
     execSync(`git add ${versionFilePath}`, { stdio: 'inherit' });
-    execSync(`git commit -m "chore: update version.ts to ${newVersion}"`, { stdio: 'inherit' });
   }
-
-  console.log(`Mixin UI version updated: ${newVersion}`);
 
   const result = await releaseChangelog({
     versionData: projectsVersionData,
