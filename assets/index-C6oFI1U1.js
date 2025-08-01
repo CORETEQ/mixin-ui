2bab191c9ea1f711d5395ef9c82bb4977986d478
vs-borodin <vs-borodin@users.noreply.github.com>
2025-08-01 14:11:45 +0000

    deploy: 9672e43c7ccdf9ca5903edc4fbe24e8ba8124720

--- assets/index-C6oFI1U1.js
+++
@@ -1,3 +0,0 @@
-import{ɵ as o,D as c,M as p,c as r,f as d,e as i,g as l,h as m}from"./index-CjNzUg7D.js";const _=`## 0.0.1-alpha.2 (2025-07-31)
-
-This was a version bump only, there were no code changes.`,e=class e{constructor(){this.data=_}};e.ɵfac=function(a){return new(a||e)},e.ɵcmp=o({type:e,selectors:[["app-changelog"]],decls:2,vars:1,consts:[[3,"data"]],template:function(a,s){a&1&&(r(0,"docs-page"),d(1,"markdown",0),i()),a&2&&(l(),m("data",s.data))},dependencies:[c,p],encapsulation:2,changeDetection:0});let t=e;export{t as default};
