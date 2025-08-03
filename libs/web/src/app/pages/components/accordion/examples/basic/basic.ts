8dafeddd24b1e66fd53aa2f012b6a91d920009c8
Vyacheslav Borodin <vb-onyx@proton.me>
2025-08-04 00:39:41 +0200

    chore: refine styles, update docs-snippet component structure

--- libs/web/src/app/pages/components/accordion/examples/basic/basic.ts
+++ libs/web/src/app/pages/components/accordion/examples/basic/basic.ts
@@ -3,7 +3,33 @@ import { XAccordion, XAccordionItem, XSlot } from '@mixin-ui/kit';
 
 @Component({
   selector: 'app-accordion-basic-example',
-  templateUrl: './basic.html',
   imports: [XAccordion, XAccordionItem, XSlot],
+  template: `
+    <x-accordion size="lg">
+      <x-accordion-item [open]="true">
+        Angular
+        <ng-template x-slot="content">
+          Angular (also referred to as Angular 2+) is a TypeScript-based free and open-source single-page web
+          application
+          framework.
+        </ng-template>
+      </x-accordion-item>
+
+      <x-accordion-item>
+        Vue.js
+        <ng-template x-slot="content">
+          Vue.js (commonly referred to as Vue; pronounced "view") is an open-source model–view–viewmodel front end
+          JavaScript framework for building user interfaces and single-page applications.
+        </ng-template>
+      </x-accordion-item>
+
+      <x-accordion-item>
+        React
+        <ng-template x-slot="content">
+          React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library.
+        </ng-template>
+      </x-accordion-item>
+    </x-accordion>
+  `
 })
 export class AccordionBasicExample {}
