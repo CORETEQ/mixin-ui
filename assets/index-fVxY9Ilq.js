207748738e198766749ef3a81954b13457a5f5aa
vs-borodin <vs-borodin@users.noreply.github.com>
2025-08-05 07:53:14 +0000

    deploy: fb59ddde82039b60fa952da591e5c6f7dc154679

---
+++ assets/index-fVxY9Ilq.js
@@ -0,0 +1,3 @@
+import{ɵ as o,D as c,M as i,c as p,g as r,e as l,i as d,j as m}from"./index-Cn0nZ1ek.js";const g=`## 0.0.1-alpha.1 (2025-08-04)
+
+This was a version bump only, there were no code changes.`,e=class e{constructor(){this.changelog=g}};e.ɵfac=function(a){return new(a||e)},e.ɵcmp=o({type:e,selectors:[["app-changelog"]],decls:2,vars:1,consts:[[3,"data"]],template:function(a,s){a&1&&(p(0,"docs-page"),r(1,"markdown",0),l()),a&2&&(d(),m("data",s.changelog))},dependencies:[c,i],encapsulation:2,changeDetection:0});let t=e;export{t as default};
