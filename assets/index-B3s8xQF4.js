cd02bcf613ad9fe4062f0e238bad73058a58d2fb
vs-borodin <vs-borodin@users.noreply.github.com>
2025-08-05 12:27:00 +0000

    deploy: 750cfafb739ef5bc22168c9ba0f439916e13e302

---
+++ assets/index-B3s8xQF4.js
@@ -0,0 +1,15 @@
+import{ɵ as o,D as c,M as s,c as m,g as d,e as l,i as p,j as r}from"./index-VfwXwc8Q.js";const h=`## 0.0.1-alpha.2 (2025-08-05)
+
+### 🚀 Features
+
+- **cdk:** add \`isTabOut\` utility ([237bfae](https://github.com/CORETEQ/mixin-ui/commit/237bfae))
+- **kit:** make \`disabled\` non-optional in calendar options ([3226592](https://github.com/CORETEQ/mixin-ui/commit/3226592))
+
+### 🩹 Fixes
+
+- **kit:** fix \`withArrow\` binding to use function call ([10ac894](https://github.com/CORETEQ/mixin-ui/commit/10ac894))
+- **kit:** fix \`disabled\` handling in calendar days grid ([7b17d1c](https://github.com/CORETEQ/mixin-ui/commit/7b17d1c))
+
+## 0.0.1-alpha.1 (2025-08-04)
+
+This was a version bump only, there were no code changes.`,n=class n{constructor(){this.changelog=h}};n.ɵfac=function(t){return new(t||n)},n.ɵcmp=o({type:n,selectors:[["app-changelog"]],decls:2,vars:1,consts:[[3,"data"]],template:function(t,i){t&1&&(m(0,"docs-page"),d(1,"markdown",0),l()),t&2&&(p(),r("data",i.changelog))},dependencies:[c,s],encapsulation:2,changeDetection:0});let a=n;export{a as default};
