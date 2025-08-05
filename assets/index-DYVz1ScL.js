99b1d37cff2f4ed4aa14f54c564071b55246ee09
vs-borodin <vs-borodin@users.noreply.github.com>
2025-08-05 00:42:03 +0000

    deploy: b3feaec920cd97aa2dbde8191950294a98524911

---
+++ assets/index-DYVz1ScL.js
@@ -0,0 +1,3 @@
+import{ɵ as o,D as c,M as p,c as r,f as i,e as l,g as d,h}from"./index-DUNJtBzD.js";const m=`## 0.0.1-alpha.1 (2025-08-04)
+
+This was a version bump only, there were no code changes.`,e=class e{constructor(){this.changelog=m}};e.ɵfac=function(a){return new(a||e)},e.ɵcmp=o({type:e,selectors:[["app-changelog"]],decls:2,vars:1,consts:[[3,"data"]],template:function(a,s){a&1&&(r(0,"docs-page"),i(1,"markdown",0),l()),a&2&&(d(),h("data",s.changelog))},dependencies:[c,p],encapsulation:2,changeDetection:0});let t=e;export{t as default};
