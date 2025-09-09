4cbec82c16b69425c5072d3c454cc97b82889e5d
vs-borodin <vs-borodin@users.noreply.github.com>
2025-09-09 07:54:15 +0000

    deploy: e462d51045b3978d7358cfcf203c91b8c8760439

---
+++ assets/index-Cugusal6.js
@@ -0,0 +1,28 @@
+import{ɵ as c,D as o,M as s,c as d,g as m,e as h,i as l,j as p}from"./index-DpmW4adQ.js";const b=`## 0.0.1-alpha.3 (2025-09-09)
+
+### 🚀 Features
+
+- **kit:** update checkbox styles ([6bd22f6](https://github.com/CORETEQ/mixin-ui/commit/6bd22f6))
+
+### 🩹 Fixes
+
+- **cdk:** handle pattern normalization in \`date\` mask to prevent symbols shift back ([543ae95](https://github.com/CORETEQ/mixin-ui/commit/543ae95))
+- **kit:** add \`aria-checked\` binding to switch for accessibility ([da572a2](https://github.com/CORETEQ/mixin-ui/commit/da572a2))
+- **kit:** fix \`disabled\` handling in checkbox ([e85ddbc](https://github.com/CORETEQ/mixin-ui/commit/e85ddbc))
+- **kit:** fix \`disabled\` handling in switch ([64bd234](https://github.com/CORETEQ/mixin-ui/commit/64bd234))
+
+## 0.0.1-alpha.2 (2025-08-05)
+
+### 🚀 Features
+
+- **cdk:** add \`isTabOut\` utility ([237bfae](https://github.com/CORETEQ/mixin-ui/commit/237bfae))
+- **kit:** make \`disabled\` non-optional in calendar options ([3226592](https://github.com/CORETEQ/mixin-ui/commit/3226592))
+
+### 🩹 Fixes
+
+- **kit:** fix \`withArrow\` binding to use function call ([10ac894](https://github.com/CORETEQ/mixin-ui/commit/10ac894))
+- **kit:** fix \`disabled\` handling in calendar days grid ([7b17d1c](https://github.com/CORETEQ/mixin-ui/commit/7b17d1c))
+
+## 0.0.1-alpha.1 (2025-08-04)
+
+This was a version bump only, there were no code changes.`,i=class i{constructor(){this.changelog=b}};i.ɵfac=function(t){return new(t||i)},i.ɵcmp=c({type:i,selectors:[["app-changelog"]],decls:2,vars:1,consts:[[3,"data"]],template:function(t,e){t&1&&(d(0,"docs-page"),m(1,"markdown",0),h()),t&2&&(l(),p("data",e.changelog))},dependencies:[o,s],encapsulation:2,changeDetection:0});let n=i;export{n as default};
