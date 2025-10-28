3cb84d7b1e8d202acd059d7cab653e17f028ac30
Vyacheslav Borodin <vb-onyx@proton.me>
2025-10-28 10:53:03 +0100

    feat(cdk): add `getActiveElement` utility function to dom utils

--- libs/cdk/utils/dom/index.ts
+++ libs/cdk/utils/dom/index.ts
@@ -1,5 +1,6 @@
 export * from './contains-focus';
 export * from './get-event-target';
+export * from './get-active-element';
 export * from './is-element';
 export * from './related-to';
 export * from './get-focusable-element';
